da72449468dcdf8a00e21cb3e329d6556b97f341
Ali Fahmi <lab@advanced-network.org>
2013-12-14 02:38:53 +0900

    Styling UI to Red-White Flag UI of Indonesia
    Signed-off-by: Ali Fahmi <lab@advanced-network.org>

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,9 +1,18 @@
 class DashboardController < ApplicationController
   before_filter :authenticate_user!
   #if user_signed_in?, current_user.full_name, etc. 
+  layout "dashboard"
   
   def index
-    #because layouts/dashboard.html.erb exists in views-->go to that
-    #else, got to dashboard/index.html.erb
+    @content = "index"
+    
   end
+  
+  def immigration
+    @content = "immigration"
+    
+  end
+  
+  
+  
 end
