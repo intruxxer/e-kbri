315d2f39976bc8d2feee78cd8991104751016ec8
Ali Fahmi <lab@advanced-network.org>
2014-01-05 04:56:34 +0900

    Visa Application Working with Mailer
    Signed-off-by: Ali Fahmi <lab@advanced-network.org>

--- app/controllers/immigration/visa_controller.rb
+++ app/controllers/immigration/visa_controller.rb
@@ -40,7 +40,7 @@ class Immigration::VisaController < ApplicationController
    
     @visa = Visa.new(post_params)
     if @visa.save then
-      current_user.visa = @visa 
+      #current_user.visa = @visa 
       UserMailer.visa_received_email(@visa).deliver
       respond_to do |format|
         format.html { redirect_to :back, :notice => "Your visa application is successfully received!" }
--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -2,15 +2,15 @@ class UserMailer < ActionMailer::Base
   # default from: "welcome-no-reply@openticket.net"
   
   def visa_received_email(visa)
-  	@url = "http://staging.openticket.net"
+  	@url = "http://kbri.seoul.kr"
   	@email = visa.email
-  	attachments["logo.png"] = File.read("#{Rails.root}/public/images/logo.png")
-  	attachments["ot-presentation-small.png"] = File.read("#{Rails.root}/public/images/ot-presentation-small.png")
+  	attachments["garuda.png"] = File.read("#{Rails.root}/public/assets/images/garuda.png")
+  	#attachments["ot-presentation-small.png"] = File.read("#{Rails.root}/public/images/ot-presentation-small.png")
   	mail(
   		:to => visa.email, 
-  		:subject => "Thank you for signing up to Open Ticket!", 
-  		:from => "Welcome to Open ticket <no-reply@openticket.net>",
-  		:reply_to => "Admin Open Ticket <admin@emonkiz.com>" 
+  		:subject => "Thank you for using e-KBRI to gain entry to Indonesia!", 
+  		:from => "Administrator of e-KBRI <admin@kbri.seoul.kr>",
+  		:reply_to => "Visa Counsel of KBRI <visa@kbri.seoul.kr>" 
   		)
   end
 end
