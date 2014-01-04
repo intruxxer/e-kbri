635184ed83d91ff2eb88d9da2738a6ebad36274b
Ali Fahmi <lab@advanced-network.org>
2014-01-05 05:15:45 +0900

    Adding user dashboard for user's own application history
    Signed-off-by: Ali Fahmi <lab@advanced-network.org>

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,18 +1,20 @@
 class WelcomeController < ApplicationController
   def index	
-    @latestdata = {}
+    @latestvisadata = {}
+    @latestreportdata = {}
   	if user_signed_in?
-  	  #visadata = Visa.where(user_id: current_user)
+  	  visadata = Visa.where(user_id: current_user)
   		reportdata = Report.where(user_id: current_user)
-  		#if visadata.count > 0
-      #  visadata.each do |t|
-      #    @latestdata["visaapply" + t.id] = {'status' => 'success', 'link' => edit_visa_path(current_user), 'name' => 'Visa Application', 'timestamp' => t.updated_at}
-      #  end
-      #end
+  		
+  		if visadata.count > 0
+        visadata.each do |t|
+          @latestvisadata["visaapply" + t.id] = {'status' => 'success', 'link' => edit_visa_path(current_user), 'name' => 'Visa Application', 'timestamp' => t.updated_at}
+        end
+      end
   		
   		if reportdata.count > 0
   			reportdata.each do |t|
-  				@latestdata["lapordiri" + t.id] = {'status' => 'success', 'link' => edit_report_path(current_user), 'name' => 'Pelaporan Data Diri', 'timestamp' => t.updated_at}
+  				@latestreportdata["lapordiri" + t.id] = {'status' => 'success', 'link' => edit_report_path(current_user), 'name' => 'Pelaporan Data Diri', 'timestamp' => t.updated_at}
   			end
   		end
   	end
