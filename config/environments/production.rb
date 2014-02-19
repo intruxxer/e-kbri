cd42a0fcd4c889ace4e2bb70f5cf436f1a75cb64
Ali Fahmi <lab@advanced-network.org>
2014-02-19 15:36:59 +0900

    Fix user mailer
    Host in devise mailer is absent in production.rb
    
    Signed-off-by: Ali Fahmi <lab@advanced-network.org>

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,7 @@
 EKbri::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-
+  
+  config.action_mailer.default_url_options = {host: "kbri.seoul.kr"}
   # Code is not reloaded between requests.
   config.cache_classes = true
 
