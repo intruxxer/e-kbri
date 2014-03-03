52a4b663e812991dab21845ac5fdb67aeb829414
Ali Fahmi PN <lab@advanced-network.org>
2014-03-03 16:11:00 +0900

    Fixing view in user panel Welcome INDEX & visa.rb
    Signed-off-by: Ali Fahmi PN <lab@advanced-network.org>

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,55 +1,39 @@
 class WelcomeController < ApplicationController
   def index	
-    @latestvisadata = {}
-    @latestreportdata = {}
-    @latestpassportdata = {}
   	if user_signed_in?
-  	  #puts "Cuser=>"+current_user.inspect
   	  visadata = Visa.where(user_id: current_user)
-  	  #puts "========================"
   	  @uservisa = Visa.where(owner_id: current_user.id)
-  	  #puts "Visa-data=>"+numvis.inspect
-  	  #puts "========================"
+  	  
   	  passportdata = Passport.where(user_id: current_user)
-      #puts "Passport-data=>"+visadata.inspect
+      @userpassport = Passport.where(user_id: current_user)#recheck
       
-  		reportdata = Report.where(user_id: current_user)
-  		#puts "Report-data=>"+reportdata.inspect
+  		@userreport = Report.where(user_id: current_user)
   		
-  		#if there is at least once visa application, then visadata == 1 as for the current user
+  		#if there is at least once visa/passport/report application, 
+  		#then visadata == 1 as for the current user
   		if visadata.count > 0
-  		  #puts "Visadata=>1 or more"
-        #visadata.each do |t|
-          #@latestvisadata["visaapply" + t.id] = {'status' => 'Success', 'link' => edit_visa_path(current_user), 'name' => 'Visa Application', 'timestamp' => t.updated_at}
-        #end
         @uservisa.each do |x|
-          @latestvisadata = Hash.new(x)
-          #puts x.ref_id.inspect
+          puts x.inspect
         end
-        puts @latestvisadata.inspect
       end
-  		puts "latestVisadata=>"+@latestvisadata.inspect
-  		
+
   		if passportdata.count > 0
-        #puts "Passport-data=>1 or more"
-        passportdata.each do |t|
-          @latestpassportdata["passportapply" + t.id] = {'status' => 'Applied', 'link' => edit_passport_path(current_user), 'name' => 'Aplikasi Paspor/SPLP', 'timestamp' => t.updated_at}
+        @userpassport.each do |x|
+          puts x.inspect
         end
-        #puts "latestPassportdata=>"+@latestpassportdata.inspect
       end
   		
-  		if reportdata.count > 0
-  			reportdata.each do |t|
-  				@latestreportdata["lapordiri" + t.id] = {'status' => 'Terdaftar', 'link' => edit_report_path(current_user), 'name' => 'Pelaporan Data Diri', 'timestamp' => t.updated_at}
+  		if @userreport.count > 0
+  			@userreport.each do |x|
+  				puts x.inspect
   			end
   		end
-  		
-  	end
+  	end#if user_signed_in?
 	
   end
   
   def concept
-    #to show the concept
+    #E-KBRI Concepts
   end
   
 end
