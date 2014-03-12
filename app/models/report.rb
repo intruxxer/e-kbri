a8a6bd804aa57267a6c583e520af487942ba3bd9
Ali Fahmi <lab@advanced-network.org>
2014-03-12 21:12:41 +0900

    Revise data to match SISARI

--- app/models/report.rb
+++ app/models/report.rb
@@ -67,7 +67,7 @@ class Report
   
   private
   def assign_ref_id
-    self.ref_id = generate_string(3)+"-"+Random.new.rand(10**4..10**10).to_s+generate_string(3)
+    self.ref_id = generate_string(3)+"/KONS/"+generate_string(3)
   end
   def generate_string(length=5)
       chars = 'abcdefghjkmnpqrstuvwxyzABCDEFGHJKLMNOPQRSTUVWXYZ123456789'
