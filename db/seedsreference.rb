809532dc41dfebd590fa5a30e72ccc470d4c874e
Ali Fahmi PN <lab@advanced-network.org>
2014-04-05 20:26:58 +0700

    add seeds reference

---
+++ db/seedsreference.rb
@@ -0,0 +1,15 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
+#
+# Examples:
+#
+#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
+#   Mayor.create(name: 'Emanuel', city: cities.first)
+
+ref = Reference.new(
+  consulat_name: 'Didik Eko Pujianto',
+  treasurer_name: 'Damayanti',
+  embassy_location: 'Seoul'  
+)
+
+ref.save!
