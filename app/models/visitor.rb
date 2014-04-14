c801ea16718ca120db91693dd13171e26874daba
Ali Fahmi PN <lab@advanced-network.org>
2014-04-15 01:35:53 +0700

    Fixing yet another geocoding mistake

--- app/models/visitor.rb
+++ app/models/visitor.rb
@@ -3,10 +3,11 @@ class Visitor
   include Mongoid::Timestamps
   include Geocoder::Model::Mongoid
   
-  field :ip_address, type: String
-  field :action,     type: String
-  field :latitude,   type: Float
-  field :longitude,  type: Float
+  field :ip_address,    type: String
+  field :action,        type: String
+  field :latitude,      type: Float
+  field :longitude,     type: Float
+  field :coordinates,   type: Array
   
   geocoded_by :ip_address, :latitude => :lat, :longitude => :lon
   after_validation :geocode
