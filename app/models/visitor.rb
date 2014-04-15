35b2b8c6f92d1380f3bf9c0ebe6ab6573de36c55
Ali Fahmi PN <lab@advanced-network.org>
2014-04-15 14:31:46 +0700

    add reverse_geocode feature

--- app/models/visitor.rb
+++ app/models/visitor.rb
@@ -4,13 +4,17 @@ class Visitor
   include Geocoder::Model::Mongoid
   
   field :who,           type: String
-  field :ip_address,    type: String
   field :action,        type: String
+  field :ip_address,    type: String
   field :coordinates,   type: Array
+  field :address,       type: String
   #Geocode needs this field to store the geo info in geocode data
   #Geocode stores geocode info in coordinates[0] as latitude, coordinates[1] as longitude)
   
   geocoded_by :ip_address, :latitude => :lat, :longitude => :lon
   after_validation :geocode
+  
+  reverse_geocoded_by :coordinates
+  after_validation :reverse_geocode
   
 end
