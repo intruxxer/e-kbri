934f9bc1d36f38e9f5831bab39574ece6a32aea5
teguhsyahmar <teguh.syahmar@gmail.com>
2014-04-23 21:25:35 +0900

    fix pnbp add check payment passport

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -79,7 +79,7 @@ class DashboardController < ApplicationController
       
       @daterange = { 'startperiod' => params[:periodical][:startperiod], 'endperiod' => params[:periodical][:endperiod] }
       
-      @visa = Visa.where(:payment_date => {'$gte' => params[:periodical][:startperiod],'$lte' => params[:periodical][:endperiod]})
+      @visa = Visa.where(:printed_date => {'$gte' => params[:periodical][:startperiod],'$lte' => params[:periodical][:endperiod]})
       @passport = Passport.where(:payment_date => {'$gte' => params[:periodical][:startperiod],'$lte' => params[:periodical][:endperiod]})
             
       @passportfee = Passportfee.all
