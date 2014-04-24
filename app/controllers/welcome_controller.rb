eed171941fd7742cf112cf5d0e3ae0e2486f39ce
Ali Fahmi PN <lab@advanced-network.org>
2014-04-24 14:54:40 +0700

    Debug Lapor Diri

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -27,8 +27,9 @@ class WelcomeController < ApplicationController
   	if user_signed_in?
   	  @userreport = Report.where(user_id: current_user.id).where(is_valid: true).desc(:updated_at)
   	  if current_user.has_role? :admin then
-  	    @uservisa = Visa.all.page(params[:page]).per(5)
-  	    @userpassport = Passport.all.page(params[:page]).per(5)
+  	    @uservisa = Visa.all.page(params[:page]).per(10)
+  	    @userpassport = Passport.all.page(params[:page]).per(10)
+  	    @userreport = Report.all.page(params[:page]).per(10)
   	    
   	  else
   	    visadata = Visa.where(user_id: current_user)
