4406417deb3abd3aece5a0ba058fe2bf1eb74134
Ali Fahmi PN <lab@advanced-network.org>
2014-06-09 01:54:33 +0700

    To escape from e.kbri.seoul.kr forced SSL

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,8 +1,13 @@
 EKbri::Application.configure do
-  config.force_ssl = true
-  config.to_prepare { Devise::SessionsController.force_ssl }
-  config.to_prepare { Devise::RegistrationsController.force_ssl }
-  config.to_prepare { Devise::PasswordsController.force_ssl }
+  if site_url == "e.kbri.seoul.kr" then
+   config.force_ssl = false
+  else
+    config.force_ssl = true
+    config.to_prepare { Devise::SessionsController.force_ssl }
+    config.to_prepare { Devise::RegistrationsController.force_ssl }
+    config.to_prepare { Devise::PasswordsController.force_ssl }  
+  end
+  
   # Settings specified here will take precedence over those in config/application.rb.
   config.action_mailer.default_url_options = {host: "id-embassy.kr"}
   # Code is not reloaded between requests.
