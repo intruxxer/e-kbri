8efb57beee4421a49086d001530bd086b0bba0b5
teguhsyahmar <teguh.syahmar@gmail.com>
2014-12-17 04:41:57 +0700

    .

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,18 +1,9 @@
 class WelcomeController < ApplicationController
    
-  def index	
-=begin
-    message_one = "Dear Visitor, Please kindly be notified that: <br/><br/>(1) The Indonesian Embassy for Republic of Korea has decided to make adjustment on fees pertaining to consular services such as Visas & Re-entry Application Fees, 
-                  effective <b>1 August 2014.</b> Please kindly check the new fees structure prior to making payment for your respective application.".html_safe
-    message_two = "<br/>(2) Starting from 18 August 2014, VISA pickup <b>is only available from 09.00 - 12.30</b>".html_safe                
-    message_three = "<br/>Thank You & Best Regards,<br/>The Indonesian Embassy for Republic of Korea."
-=end
+  def index
     message_one = "<b>PEMBERITAHUAN/공지 사항/NOTIFICATION</b>".html_safe
-    message_two = "<br/>PELAYANAN KONSULER KBRI SEOUL DAN KUKRI BUSAN AKAN TUTUP PADA TANGGAL <b>22 - 31 DESEMBER 2014 DAN 1 JANUARI 2015</b>. PELAYANAN KONSULER AKAN DIBUKA KEMBALI PADA TANGGAL <b>2 JANUARI 2015</b>.
-PEMBAYARAN BIAYA PELAYANAN KEKONSULERAN HARUS DILAKUKAN PALING LAMBAT PADA TANGGAL <b>19 DESEMBER 2014</b>.".html_safe
-    message_three = "<br/>주한 인도네시아 대사관과 부산 인도네시아 영사관 연말 접수 마감일은 <b>12월 19일까지</b> 입니다. 
-<b>2014년 12월 22일부터 12월 31일까지는</b> 비자 접수가 불가합니다.
-. <b>1월 1일부터</b> 정상근무 합니다.".html_safe
+    message_two = "<br/>PELAYANAN KONSULER KBRI SEOUL DAN KUKRI BUSAN AKAN TUTUP PADA TANGGAL <b>22 - 31 DESEMBER 2014 DAN 1 JANUARI 2015</b>. PELAYANAN KONSULER AKAN DIBUKA KEMBALI PADA TANGGAL <b>2 JANUARI 2015</b>.PEMBAYARAN BIAYA PELAYANAN KEKONSULERAN HARUS DILAKUKAN PALING LAMBAT PADA TANGGAL <b>19 DESEMBER 2014</b>.".html_safe
+    message_three = "<br/>주한 인도네시아 대사관과 부산 인도네시아 영사관 연말 접수 마감일은 <b>12월 19일까지</b> 입니다.<b>2014년 12월 22일부터 12월 31일까지는</b> 비자 접수가 불가합니다.<b>1월 1일부터</b> 정상근무 합니다.".html_safe
     message_four = "<br/>THE INDONESIAN EMBASSY IN SEOUL AND THE CONSULAR OFFICE IN BUSAN WILL BE CLOSED FROM <b>DECEMBER 22-31, 2014 AND JANUARY 1, 2015</b>. WE WILL REOPEN ON <b>JANUARY 2, 2015</b>. PAYMENT FOR ALL CONSULAR SERVICE FEE MUST BE PAID THE LATEST ON <b>DECEMBER 19, 2014</b>.".html_safe
     warning = [ message_one, message_two, message_three, message_four ]
     flash[:warning] = warning.join("<br/>").html_safe
