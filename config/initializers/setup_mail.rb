b3ec175122eadbbdc5258f19e568134e22d3117b
Ali Fahmi <lab@advanced-network.org>
2015-04-01 15:25:29 +0700

    Update setup_mail.rb
    Github abis kontrak

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -2,8 +2,8 @@ ActionMailer::Base.smtp_settings = {
   :address              => "smtp.mandrillapp.com",  
   :port                 => 587,  
   :domain               => "id-embassy.kr",  
-  :user_name            => "admin@kbri.seoul.kr",  
-  :password             => "5djW6R_-NI-Ia-2tS58T5g",  
+  :user_name            => "x@kbri.seoul.kr",  
+  :password             => "xxXXXxxx",  
   :authentication       => "plain",  
   :enable_starttls_auto => true  
 }
